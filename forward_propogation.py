9b7d86d5412973b154108d3da5b77270ef4d7719
Vishwa GW (Randy) <136871744+vishwagw@users.noreply.github.com>
2025-04-20 06:43:15 +0530

    Update forward_propogation.py

--- forward_propogation.py
+++ forward_propogation.py
@@ -6,3 +6,14 @@ def forward_prop(w1, w2, x):
     z2 = np.dot(w2, a1)
     a2 = sigmoid(z2)
     return z1, a1, z2, a2
+# Backward propagation
+def back_prop(m, w1, w2, z1, a1, z2, a2, y):
+
+    dz2 = a2-y
+    dw2 = np.dot(dz2, a1.T)/m
+    dz1 = np.dot(w2.T, dz2) * a1*(1-a1)
+    dw1 = np.dot(dz1, total_input.T)/m
+    dw1 = np.reshape(dw1, w1.shape)
+
+    dw2 = np.reshape(dw2,w2.shape)
+    return dz2,dw2,dz1,dw1
